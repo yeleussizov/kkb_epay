f1a5bfe19c2cbe94c62e274c5dcf4d7eb840a77d
20th (Victor Nikulshin) <20th@master>
2012-11-19 02:17:00 +0600

    KkbEpay_Checker class.

---
+++ classes/KkbEpay_Checker.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Signatures checker.
+ */
+final class KkbEpay_Checker
+{
+
+  private $_certificate;
+
+
+  public function __construct(KkbEpay_BankCertificateInterface $cetrificate = NULL)
+  {
+    $resource = openssl_get_publickey(trim($certificate->getCertificate()));
+    if (empty($resource)) {
+      throw new KkbEpay_Exception('Provided certificate could not be loaded by openssl_get_publickey().');
+    }
+
+    $this->_certificate = $resource;
+  }
+
+  /**
+   * Checks given message with loaded public certificate.
+   *
+   * @param string $message
+   *   The original message whose signature must be verified.
+   *   Cannot be empty.
+   *
+   * @param string $signature
+   *   Verified signature as a binary string.
+   *
+   * @return boolean
+   *   TRUE if signature is corrent.
+   *   FALSE if signature was not verified.
+   */
+  public function check($message, $signature)
+  {
+    if (empty($message)) {
+      throw new KkbEpay_Exception('Message cannot be empty.');
+    }
+
+    return openssl_verify($message, $signature, $this->_certificate);
+  }
+
+}
+
