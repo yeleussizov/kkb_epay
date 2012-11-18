3688cd87415ae6735d55aefdf45d36589bd2eb6d
20th (Victor Nikulshin) <20th@master>
2012-11-19 01:18:30 +0600

    KkbEpay_Key methods' definitions.

--- classes/KkbEpay_Key.php
+++ classes/KkbEpay_Key.php
@@ -1,6 +1,137 @@
 <?php
 
-class KkbEpay_Key
+/**
+ * Representation of a RSA private key that can be used to sign and validate
+ * text messages.
+ */
+final class KkbEpay_Key
 {
+
+  private $_key;
+
+  private $_password;
+
+  private $_merchant_id;
+
+  private $_merchant_name;
+
+  private $_certificate_id;
+
+
+  public function __construct($key = NULL, $pwd = NULL, $c_id = NULL, $m_id = NULL, $m_name = NULL)
+  {
+    if ($key) {
+      $this->setKey($key);
+    }
+    if ($pwd) {
+      $this->setPassword($pwd);
+    }
+    if ($c_id) {
+      $this->setCertificateId($c_id);
+    }
+    if ($m_id) {
+      $this->setMerchantId($m_id);
+    }
+    if ($m_name) {
+      $this->setMerchantName($m_name);
+    }
+  }
+
+  public function isValid()
+  {
+    if (empty($this->_key)) return FALSE;
+    if (empty($this->_password)) return FALSE;
+    if (empty($this->_merchant_id)) return FALSE;
+    if (empty($this->_merchant_name)) return FALSE;
+    if (empty($this->_certificate_id)) return FALSE;
+
+    return TRUE;
+  }
+
+  public function setKey($key)
+  {
+    if (!is_string($key)) {
+      throw new KkbEpay_KeyError('Key must be a string.');
+    }
+    if (substr($key, 0, 31) != '-----BEGIN RSA PRIVATE KEY-----') {
+      throw new KkbEpay_KeyError('Key does not start a correct RSA key declaration.');
+    }
+    if (substr($key, -29) != '-----END RSA PRIVATE KEY-----') {
+      throw new KkbEpay_KeyError('Key does not end a correct RSA key declaration.');
+    }
+    $this->_key = trim($key);
+    return $this;
+  }
+
+  public function setPassword($pwd)
+  {
+    if (!is_string($pwd)) {
+      throw new KkbEpay_KeyError('Password must be a string.');
+    }
+    $this->_password = $pwd;
+    return $this;
+  }
+
+  public function setMerchantId($id)
+  {
+    if (!is_string($id)) {
+      throw new KkbEpay_KeyError('Merchant ID must be a string.');
+    }
+    if (!preg_match('/^[0-9]{8}$/', $id)) {
+      throw new KkbEpay_KeyError('Merchant ID does not much expected format.');
+    }
+    $this->_merchant_id = $id;
+    return $this;
+  }
+
+  public function setMerchantName($name)
+  {
+    if (!is_string($name)) {
+      throw new KkbEpay_KeyError('Merchant name must be a string.');
+    }
+    if (!preg_match('/^[A-Za-z0-9 _-]{255}$/', $name)) {
+      throw new KkbEpay_KeyError('Merchant name does not much expected format.');
+    }
+    $this->_merchant_name = $name;
+    return $this;
+  }
+
+  public function setCertificateId($id)
+  {
+    if (!is_string($id)) {
+      throw new KkbEpay_KeyError('Certificate ID must be a string.');
+    }
+    if (!preg_match('/^[A-Fa-F0-9]{10}$/', $id)) {
+      throw new KkbEpay_KeyError('Certificate ID does not much expected format.');
+    }
+    $this->_certificate_id = strtoupper($id);
+    return $this;
+  }
+
+  public function getKey()
+  {
+    return $this->_key;
+  }
+
+  public function getPasword()
+  {
+    return $this->_password;
+  }
+
+  public function getMerchantId()
+  {
+    return $this->_merchant_id;
+  }
+
+  public function getMerchantName()
+  {
+    return $this->_merchant_name;
+  }
+
+  public function getCertificateId()
+  {
+    return $this->_certificate_id;
+  }
+
 }
 
