9c62bab55a3803ae8f805fafe9b4f6e749e889f1
20th (Victor Nikulshin) <20th@master>
2012-11-19 14:26:40 +0600

    KkbEpay_DrupalKeyLoader.

--- classes/KkbEpay_DrupalKeyLoader.php
+++ classes/KkbEpay_DrupalKeyLoader.php
@@ -1,6 +1,56 @@
 <?php
 
+/**
+ * Main key loader that fetches private keys from Drupal's variables system
+ * where it is stored by the 'kkb_epay' module.
+ */
 class KkbEpay_DrupalKeyLoader implements KkbEpay_KeyLoaderInterface
 {
+
+  protected $_debug = FALSE;
+
+
+  public function getKey()
+  {
+    $data = $this->_loadKeyData();
+    return new KkbEpay_Key(
+      $data['key'],
+      $data['password'],
+      $data['certificate_id'],
+      $data['merchant_id'],
+      $data['merchant_name']
+    );
+  }
+
+  public function setDebug($flag)
+  {
+    $this->_debug = (bool) $flag;
+  }
+
+  public function verifyKey()
+  {
+    return $this->getKey()->isValid();
+  }
+
+
+  protected function _loadKeyData()
+  {
+    $variant = 'kkb_epay_private_key_' . ($this->_debug ? 'debug' : 'live');
+
+    if (!($key_data = variable_get($variant, NULL))) {
+      throw new KkbEpay_KeyException('Private key does not exist as a system variable.');
+    }
+    if (!($unpacked = $this->_unpack($key_data))) {
+      throw new KkbEpay_KeyException('Private key could not be unpacked.');
+    }
+
+    return $unpacked;
+  }
+
+  protected function _unpack($data)
+  {
+    return unserialize(base64_decode(preg_replace('/\s/', '', $data)));
+  }
+
 }
 
