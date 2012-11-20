4933820eb99781a3ce151cf764dbbdc5b9f12e63
20th (Victor Nikulshin) <20th@master>
2012-11-20 12:25:56 +0600

    Specifying 'no-whitespace' requirement on certificates.

--- classes/KkbEpay_BankCertificateInterface.php
+++ classes/KkbEpay_BankCertificateInterface.php
@@ -7,7 +7,8 @@ interface KkbEpay_BankCertificateInterface
 {
 
   /**
-   * Must return a valid certificate as a string.
+   * Must return a valid certificate as a string. It must not have any
+   * whitespaces at the beginning or the end the string.
    */
   public function getCertificate();
 
