9f7718ed2b56fe1bdcba722aa89efcea2e8d2ffa
20th (Victor Nikulshin) <20th@master>
2012-11-23 14:02:39 +0600

    API file with a list of provided hooks.

---
+++ kkb_epay.api.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file kkb_epay.api.php
+ *   Hooks provided by the kkb_epay module.
+ */
+
+function hook_kkb_epay_payment(KkbEpay_PaymentNotification $notification) {
+}
+
+function hook_kkb_epay_raw_success_response($document) {
+}
+
+function hook_kkb_epay_success_page($id) {
+}
+
+function hook_kkb_epay_failure_page($id) {
+}
+
+function hook_form_kkb_epay_billing_statement_alter(&$form, &$form_state) {
+}
+
