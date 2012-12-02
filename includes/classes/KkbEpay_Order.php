193317712a7124ba77df6eb003570b9fc2e8b280
20th (Victor Nikulshin) <20th@alahost.net>
2012-12-02 21:13:54 +0600

    Add copyright to all classes

--- includes/classes/KkbEpay_Order.php
+++ includes/classes/KkbEpay_Order.php
@@ -1,6 +1,27 @@
 <?php
 
 /**
+ * @file
+ * Copyright (C) 2012, Victor Nikulshin
+ *
+ * This file is part of KKB Epay.
+ *
+ * KKB Epay is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * KKB Epay is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with KKB Epay.  If not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+/**
  * Representation of an abstract order that will be payed using KKB Epay
  * processing center. Contains an absolute minimal amount on information to
  * create a payment document for the processing center.
